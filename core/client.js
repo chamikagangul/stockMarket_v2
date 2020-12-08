010693c2accc5a265ae378144e7207d4bf8cdbf5
Chamika_112085 <Chamika.Mahiepala@axiatadigitallabs.com>
2020-12-08 20:47:01 +0530

    bug fix

--- core/client.js
+++ core/client.js
@@ -1,5 +1,5 @@
 module.exports.start = async function () {
-    
+
     symbols = require("../data/symbols.json")
     console.log("inside client");
     const fetch = require('node-fetch');
@@ -7,7 +7,7 @@ module.exports.start = async function () {
     // symbols = symbols.slice(0,10000);
     iterifyArr(symbols);
 
-    var Core =require("./core")
+    var Core = require("./core")
     let core = new Core();
 
     var query = setInterval(() => {
@@ -15,28 +15,32 @@ module.exports.start = async function () {
         if (s == false) {
             clearInterval(query);
         }
-        try {
-            fetch("http://query1.finance.yahoo.com/v7/finance/quote?symbols=" + s)
-                .then(res => res.json())
-                .then(data => {
+
+        fetch("http://query1.finance.yahoo.com/v7/finance/quote?symbols=" + s)
+            .then(res => res.json())
+            .then(data => {
+                try {
                     symbol = data.quoteResponse.result[0].symbol;
                     open_ = data.quoteResponse.result[0].regularMarketOpen;
-                    marketPrice =  data.quoteResponse.result[0].regularMarketPrice;
+                    marketPrice = data.quoteResponse.result[0].regularMarketPrice;
                     change = data.quoteResponse.result[0].regularMarketChange;
                     changePrecentage = data.quoteResponse.result[0].regularMarketChangePercent;
 
-                    d={
+                    d = {
                         "s": symbol,
-                        "o":open_,
-                        "p":marketPrice,
-                        "c":change,
-                        "cp":changePrecentage
+                        "o": open_,
+                        "p": marketPrice,
+                        "c": change,
+                        "cp": changePrecentage
                     }
+
                     core.DATA.push(d);
-                })
-        } catch {
-            console.log(s);
-        }
+                } catch {
+                    console.log(s);
+                }
+
+            })
+
     }, 1)
 }
 
